d57bb634bbfef6062223fa2641f446cf539a4c86
Azlan <azlan@kalohq.com>
2018-04-26 08:38:23 +0100

    System import calls
    Signed-off-by: Azlan <azlan@kalohq.com>

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 const button = document.createElement('button');
 button.innerText = 'Click me';
 button.onclick = () => {
-    System.import('./image_viewer').then(module => {
-        console.log(module);
-    });
+  System.import('./image_viewer').then(module => {
+    module.default();
+  });
 };
 
 document.body.appendChild(button);
